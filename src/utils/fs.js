0afe3161885e8509ba15433036776e8d18ff09e0
Joakim Hedlund <contact@joakimhedlund.com>
2019-08-11 19:24:06 +0200

    async fs utils

---
+++ src/utils/fs.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const fs = require('fs')
+const { promisify } = require('util')
+const readDirAsync = promisify(fs.readdir)
+const readFileAsync = promisify(fs.readFile)
+const writeFileAsync = promisify(fs.writeFile)
+
+const getDirectoryFiles = async (directoryPath) => {
+  // Note: withFileTypes requires Node 10+
+  const dir = await readDirAsync(directoryPath, { withFileTypes: true })
+  return dir
+    .filter(item => !item.isDirectory())
+    .map(item => item.name)
+}
+
+module.exports = {
+  getDirectoryFiles,
+  readFileAsync,
+  writeFileAsync,
+}
