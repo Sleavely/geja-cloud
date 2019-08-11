d0b57899fad15ced1bd8981c494cd24e37f4dee4
Joakim Hedlund <contact@joakimhedlund.com>
2019-08-11 20:14:34 +0200

    WIP emailHandler

---
+++ src/email.js
@@ -0,0 +1,11 @@
+
+const Handlebars = require('handlebars')
+const path = require('path')
+const { readFileAsync } = require('utils/fs')
+
+exports.renderReceipt = async (templateVariables = {}) => {
+  // TODO: this could be optimized by using global variables for in-memory caching
+  const template = await readFileAsync(path.join(__dirname, 'emails', 'receipt.html'), { encoding: 'utf8' })
+  const renderReceipt = Handlebars.compile(template)
+  return renderReceipt(templateVariables)
+}
---
+++ src/emailHandler.js
@@ -0,0 +1,19 @@
+
+const { renderReceipt } = require('./email')
+
+exports.handler = async (event, context) => {
+  // TODO: import makeLogger from its-batches
+
+  const templateVariables = {
+    customer: {
+      firstname: 'Jay',
+      lastname: 'Alco',
+    },
+    purchase_date: 'YYYY-MM-DD',
+    future_discounts: [],
+  }
+
+  // Map event data to an object that
+
+  return renderReceipt({ user: templateVariables })
+}
