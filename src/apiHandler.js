9a0fdd05e8e2484817328b0c3982ebf9c3087208
Joakim Hedlund <contact@joakimhedlund.com>
2019-08-12 18:02:40 +0200

    Only load complete set of routes in the REST lambda; let others load logger without require-ing Stripe

--- src/apiHandler.js
+++ src/apiHandler.js
@@ -1,7 +1,13 @@
 
 const api = require('./utils/api')
 
-// Declare your Lambda handler
+api.register(require('./routes/checkout'), { prefix: '/checkout' })
+
+api.register(require('./routes/stripe'), { prefix: '/stripe' })
+api.register(require('./routes/contentful'), { prefix: '/contentful' })
+api.register(require('./routes/contentful'))
+
+// Declare actual Lambda handler
 exports.handler = async (event, context) => {
   // Run the request
   return api.run(event, context)
--- src/utils/api.js
+++ src/utils/api.js
@@ -31,12 +31,6 @@ api.use((err, req, res, next) => {
   next(err)
 })
 
-api.register(require('../routes/checkout'), { prefix: '/checkout' })
-
-api.register(require('../routes/stripe'), { prefix: '/stripe' })
-api.register(require('../routes/contentful'), { prefix: '/contentful' })
-api.register(require('../routes/contentful'))
-
 api.get('/routes', async () => {
   return api.routes()
 })
