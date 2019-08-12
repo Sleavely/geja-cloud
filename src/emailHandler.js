ab722cb6f522cb4911c07260e406e0e21e8ad64e
Joakim Hedlund <contact@joakimhedlund.com>
2019-08-12 20:52:39 +0200

    _PROBABLY_ send emails

--- src/emailHandler.js
+++ src/emailHandler.js
@@ -1,22 +1,56 @@
-
+const {
+  AWS_REGION = 'eu-west-1',
+} = process.env
+const SES = require('aws-sdk/clients/ses')
+const emailClient = new SES({ apiVersion: '2010-12-01', region: AWS_REGION })
 const makeLogger = require('./utils/logger')
 const { renderReceipt } = require('./email')
 
 exports.handler = async (event, context) => {
   const logger = makeLogger(event, context)
   logger.debug('Emailhandler was called!', { event, context })
-  // TODO: import makeLogger from its-batches
 
-  const templateVariables = {
-    customer: {
-      firstname: 'Jay',
-      lastname: 'Alco',
-    },
-    purchase_date: 'YYYY-MM-DD',
-    future_discounts: [],
-  }
+  // Some fuckery to unwrap the order from SNS message from Lambda-managed SQS batch
+  return Promise.all(event.Records.map(async (sqsMessage) => {
+    const snsNotification = JSON.parse(sqsMessage.body)
+    const order = JSON.parse(snsNotification.Message)
 
-  // Map event data to an object that
+    // Map event data to an object that corresponds to what the template expects
+    const templateVariables = {
+      receipt_id: order.id,
+      customer: order.customer,
+      purchase_date: (new Date(order.createdAt)).toLocaleDateString('sv-SE'),
+    }
+    const htmlBody = await renderReceipt(templateVariables)
 
-  return renderReceipt({ user: templateVariables })
+    // Send it
+    const sesParams = {
+      Destination: {
+        ToAddresses: [
+          order.customer.email,
+        ],
+      },
+      Message: {
+        Body: {
+          Html: {
+            Charset: 'UTF-8',
+            Data: htmlBody,
+          },
+        },
+        Subject: {
+          Charset: 'UTF-8',
+          Data: 'Orderbekräftelse',
+        },
+      },
+      Source: 'info@geja.se',
+      ReplyToAddresses: [
+        'info@geja.se',
+      ],
+    }
+    const emailResponse = await emailClient.sendEmail(sesParams).promise()
+    logger.debug('Got response from SES sendMail()', { emailResponse })
+  })).catch((err) => {
+    logger.error('Could not send email.', { error: err })
+    return Promise.reject(err)
+  })
 }
