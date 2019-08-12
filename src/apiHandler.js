560b6d78c398c732d54505726b4d333925b75a2b
Joakim Hedlund <contact@joakimhedlund.com>
2019-08-12 23:41:07 +0200

    Remove some empty lines because

--- src/apiHandler.js
+++ src/apiHandler.js
@@ -2,7 +2,6 @@
 const api = require('./utils/api')
 
 api.register(require('./routes/checkout'), { prefix: '/checkout' })
-
 api.register(require('./routes/stripe'), { prefix: '/stripe' })
 api.register(require('./routes/contentful'), { prefix: '/contentful' })
 api.register(require('./routes/contentful'))
