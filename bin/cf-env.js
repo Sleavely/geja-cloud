81bd0b6b047e2d84adfab8b4b73e97fc996ec61c
Joakim Hedlund <contact@joakimhedlund.com>
2019-08-25 08:44:24 +0200

    Support loading env-based .env, let existing vars take preccedence

--- bin/cf-env.js
+++ bin/cf-env.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 const dotenv = require('dotenv')
+const path = require('path')
 
-const env = dotenv.config().parsed
+const env = dotenv.config({
+  path: path.resolve(process.cwd(), `.env${process.env.ENVIRONMENT ? `.${process.env.ENVIRONMENT}` : ''}`)
+}).parsed
 
-Object.keys(env).forEach((key) => {
-  console.log(`${key.replace(/_/g, '')}=${env[key]}`)
+Object.entries(env).forEach(([key, value]) => {
+  console.log(`${key.replace(/_/g, '')}=${process.env[key] || value}`)
 })
