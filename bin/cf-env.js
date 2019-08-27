e65ee052bb55d87ddc24a96b6b7faa0e8db8c6fc
Joakim Hedlund <contact@joakimhedlund.com>
2019-08-27 10:34:40 +0200

    Make .env a defaulty store, with environmentable overrides

--- bin/cf-env.js
+++ bin/cf-env.js
@@ -3,9 +3,17 @@
 const dotenv = require('dotenv')
 const path = require('path')
 
-const env = dotenv.config({
-  path: path.resolve(process.cwd(), `.env${process.env.ENVIRONMENT ? `.${process.env.ENVIRONMENT}` : ''}`),
-}).parsed
+const environmentSpecificEnv = (
+  process.env.ENVIRONMENT && dotenv.config({
+    path: path.resolve(process.cwd(), `.${process.env.ENVIRONMENT}.env`),
+  }).parsed
+) || {}
+
+const defaultEnv = dotenv.config({
+  path: path.resolve(process.cwd(), `.env`),
+}).parsed || {}
+
+const env = { ...environmentSpecificEnv, ...defaultEnv }
 
 Object.entries(env).forEach(([key, value]) => {
   console.log(`${key.replace(/_/g, '')}=${process.env[key] || value}`)
