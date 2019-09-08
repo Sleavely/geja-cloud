dc556113a1f75cceff858e5c89794be210c83b7d
Joakim Hedlund <contact@joakimhedlund.com>
2019-09-08 12:09:49 +0200

    CI support for CloudFormation parameters

--- bin/cf-env.js
+++ bin/cf-env.js
@@ -9,11 +9,21 @@ const environmentSpecificEnv = (
   }).parsed
 ) || {}
 
+// The most common values
 const defaultEnv = dotenv.config({
   path: path.resolve(process.cwd(), `.env`),
 }).parsed || {}
 
-const env = { ...environmentSpecificEnv, ...defaultEnv }
+// For CI values will be in env already,
+// but we need to output the right keys by looking at .example.env
+const fallbackEnv = dotenv.config({
+  path: path.resolve(process.cwd(), `.example.env`),
+}).parsed || {}
+
+// The order of spreading isn't that important here because dotenv has
+// already assigned stuff to process.env in the right order.
+// However, for the sake of clarity its behavior is replicated here.
+const env = { ...fallbackEnv, ...defaultEnv, ...environmentSpecificEnv }
 
 Object.entries(env).forEach(([key, value]) => {
   console.log(`${key.replace(/_/g, '')}=${process.env[key] || value}`)
